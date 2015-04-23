4f171b20491788c56b1af535b01ee6f5ef677761
Clyde G Marl <marl.clyde@gmail.com>
2015-04-22 20:26:12 -0700

    show purchased stocks

--- client/index.js
+++ client/index.js
@@ -6,33 +6,37 @@ angular.module('day-trader', ['firebase'])
 }])
 .controller('master',['$scope','$firebaseObject','$firebaseArray','$http',function($scope,$firebaseObject,$firebaseArray,$http){
 
-  $scope.getQuote = function(){
-    debugger;
-    console.log($http.jsonp('http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol='+($scope.stock.symbol)+'&callback=JSON_CALLBACK'));
-      //.then(function(response){
-      //return response
-  };
-
-
-  var fbPortfolios = $scope.fbRoot.child('portfolios');
-  var afPortfolios = $firebaseObject(fbPortfolios);
-  $scope.portfolios = afPortfolios;
-
   var fbUser = $scope.fbRoot.child('user');
   var afUser = $firebaseObject(fbUser);
   $scope.user = afUser;
+
+  var fbPortfolios = $scope.fbRoot.child('technology');
+  var afPortfolios = $firebaseArray(fbPortfolios);
+  $scope.technology = afPortfolios;
+
+  var fbPortfolios2 = $scope.fbRoot.child('medical');
+  var afPortfolios2 = $firebaseArray(fbPortfolios2);
+  $scope.medical = afPortfolios2;
+
+  var fbPortfolios3 = $scope.fbRoot.child('realestate');
+  var afPortfolios3 = $firebaseArray(fbPortfolios3);
+  $scope.realestate = afPortfolios3;
 
   $scope.saveUser = function(){
     $scope.user.$save();
   };
 
   $scope.buyStock = function(){
-    $scope.getQuote();
-    // var stock = {
-    //   name: 'technology',
-    //   sym: $scope.stock.symbol,
-    //   shares: $scope.stock.shares
-    // };
-    $scope.portfolios.$ad();
+    $http.jsonp('http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol='+$scope.stock.symbol+'&callback=JSON_CALLBACK').then(function(response){
+      var stock = {};
+      stock.shares = $scope.stock.shares;
+      stock.symbol = $scope.stock.symbol;
+      stock.price = (response.data.LastPrice);
+
+      if ($scope.stock.portfolio){
+        $scope[$scope.stock.portfolio].$add(stock);
+      } else {alert('Please pick a portfolio and try again.');}
+    });
   };
+
 }]);
